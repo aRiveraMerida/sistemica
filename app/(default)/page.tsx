f055ebc34d000af4102d9fdeb38f85ac3e6ff074
Alberto Rivera <albertorivera@MacBook-Pro-de-Alberto.local>
2023-05-17 23:26:28 +0200

    feat:initial commit

---
+++ app/(default)/page.tsx
@@ -0,0 +1,25 @@
+export const metadata = {
+  title: 'Home - Simple',
+  description: 'Page description',
+};
+
+import Hero from '@/app/components/hero';
+import Features from '@/app/components/features';
+import FeaturesBlocks from '@/app/components/features-blocks';
+import Testimonials from '@/app/components/testimonials';
+import Newsletter from '@/app/components/newsletter';
+import Acelerator from '../components/Acelerator';
+import Team from '../components/Team';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturesBlocks />
+      <Acelerator />
+      <Testimonials />
+      <Team />
+      <Newsletter />
+    </>
+  );
+}
